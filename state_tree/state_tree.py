b56003350b6eaf1e94f95d1d5af3439c4fd9e13e
SamTheBestFishy <samleigh.derp@gmail.com>
2023-08-22 21:45:27 -0700

    knot cases setup

--- state_tree/state_tree.py
+++ state_tree/state_tree.py
@@ -10,10 +10,23 @@ class state_tree:
         self.states = [[]]*self.height
 
     # TODO: adjust for offset or odd number of strings
-    def check_validity(self, string_sequence, height):
-        knots = self.pattern[height]
-        preknots = [ string_sequence[i:i+2] for i in range(0, self.strings, 2) ]
+    def check_validity(self, string_sequence, row):
+
+        knots = self.pattern[row]
+
+        if string_sequence % 2 == 1: #For bracelets with odd strings
+            if row % 2 == 0:
+                preknots = [ string_sequence[i:i+2] for i in range(0, self.strings-1, 2) ]
+            else:
+                preknots = [ string_sequence[i:i+2] for i in range(1, self.strings, 2) ]
+        else:
+            if row % 2 == 0:
+                preknots = [ string_sequence[i:i+2] for i in range(0, self.strings, 2) ]
+            else:
+                preknots = [ string_sequence[i:i+2] for i in range(1, self.strings-1, 2) ]
+
         for i in range(self.width):
             if knots[i] not in preknots[i]:
                 return False
         return True
+
