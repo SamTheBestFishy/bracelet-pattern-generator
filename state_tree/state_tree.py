7c24e01a1bd51680e03b33965af3404818026823
gurk314 <gurkeerat.chhina@gmail.com>
2023-08-22 23:42:18 -0700

    some alternate versions of algorithm in comments

--- state_tree/state_tree.py
+++ state_tree/state_tree.py
@@ -30,12 +30,20 @@ class state_tree:
             if knots[i] not in preknots[i]:
                 return False
         return True
+    
+
     # TODO: try to parallelize the checking
     def check_validity_faster_maybe(self, string_sequence, row):
         knots = self.pattern[row]
         preknots = [ string_sequence[i:i+2] for i in range(row%2, self.strings-(row+self.strings)%2, 2) ]
-        return all([knots[i] in preknots[i] for i in range(self.width-((row+1)*self.strings)%2)])
-        # for i in range(self.width-((row+1)*self.strings)%2):
+        return all([knots[i] in preknots[i] for i in range(self.width-(row*(self.strings+1))%2)])
+    
+        # knots = self.pattern[row]
+        # if row%2 == 1 and self.strings%2 == 0:
+        #   knots.pop()
+        # preknots = [ string_sequence[i:i+2] for i in range(row%2, self.strings-(row+self.strings)%2, 2) ]
+        # for i in len(knots):
         #     if knots[i] not in preknots[i]:
         #         return False
         # return True
+        # # return all([knots[i] in preknots[i] for i in len(knots)])
