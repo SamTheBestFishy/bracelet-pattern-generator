9bd9222fefe8cd7b8210f04d586ea653275af8f1
gurk314 <gurkeerat.chhina@gmail.com>
2023-08-22 23:28:47 -0700

    Added alternate validity method

--- state_tree/state_tree.py
+++ state_tree/state_tree.py
@@ -9,24 +9,32 @@ class state_tree:
             self.strings = strings
         self.states = [[]]*self.height
 
-    # TODO: adjust for offset or odd number of strings
+    # TODO: try to parallelize the checking
     def check_validity(self, string_sequence, row):
 
         knots = self.pattern[row]
 
-        if string_sequence % 2 == 1: #For bracelets with odd strings
-            if row % 2 == 0:
-                preknots = [ string_sequence[i:i+2] for i in range(0, self.strings-1, 2) ]
-            else:
-                preknots = [ string_sequence[i:i+2] for i in range(1, self.strings, 2) ]
-        else:
-            if row % 2 == 0:
-                preknots = [ string_sequence[i:i+2] for i in range(0, self.strings, 2) ]
-            else:
-                preknots = [ string_sequence[i:i+2] for i in range(1, self.strings-1, 2) ]
+        if self.strings % 2 == 1 and row % 2 == 0: #For bracelets with odd strings
+            preknots = [ string_sequence[i:i+2] for i in range(0, self.strings-1, 2) ]
+        elif self.strings % 2 == 1 and row % 2 == 1:
+            preknots = [ string_sequence[i:i+2] for i in range(1, self.strings, 2) ]
+        elif self.strings % 2 == 0 and row % 2 == 0:
+            preknots = [ string_sequence[i:i+2] for i in range(0, self.strings, 2) ]
+        elif self.strings % 2 == 0 and row % 2 == 1:
+            preknots = [ string_sequence[i:i+2] for i in range(1, self.strings-1, 2) ]
+        else: # This should never happen
+            raise Exception("self.strings or row is not an integer")
 
+        # TODO: even strings, even row case        
         for i in range(self.width):
             if knots[i] not in preknots[i]:
                 return False
         return True
 
+    def check_validity_faster_maybe(self, string_sequence, row):
+        knots = self.pattern[row]
+        preknots = [ string_sequence[i:i+2] for i in range(row%2, self.strings-(row+self.strings)%2, 2) ]
+        for i in range(self.width-((row+1)*self.strings)%2):
+            if knots[i] not in preknots[i]:
+                return False
+        return True
